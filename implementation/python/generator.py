78f123072fadd965cb37a3c5336c108225e06adf
Marius Wichtner <mwichtner@coconut-capital.com>
2017-12-02 12:15:45 +0100

    fixed generator memory issue

--- implementation/python/generator.py
+++ implementation/python/generator.py
@@ -9,5 +9,6 @@ to numbers.npy as binary numpy array
 '''
 
 size = int(sys.argv[1]) if len(sys.argv) > 1 else 128
-random_range = np.random.choice(np.arange(size), size, replace=False)
+random_range = np.arange(size)
+np.random.shuffle(random_range)
 np.save(constants.NUMBER_FILE, random_range)
