85eee0bfdc61e21aadc2c63afc53da3779ca2964
eren <e.albayrak@it-frankfurt.com>
2018-01-21 14:53:31 +0100

    High performance improvements.

--- implementation/c++/OddEvenSort.cpp
+++ implementation/c++/OddEvenSort.cpp
@@ -10,172 +10,134 @@ using namespace std;
 /**
  * @param binary_file: the binary file on Disk.
  * @param vector: the values from binary file will store in the vector.
+ * @return EXIT_SUCCESS or EXIT_FAILURE
  * */
-void fill_vector_from_binary_file(vector<int> *vector,
+int fill_vector_from_binary_file(vector<int> *vector,
                                   char *binary_file,
                                   long rank,
-                                  int count_nodes)
+                                  int count_processes)
 {
     ifstream bin_file(binary_file, ios::in | ios::binary);
     bin_file.seekg(0, ios::end);
 
-    long int count_all_bytes = bin_file.tellg();
-    long int vector_size     = (count_all_bytes / sizeof(int) / count_nodes);
+    const long int count_all_bytes  = bin_file.tellg();
+    const unsigned long vector_size = (count_all_bytes / sizeof(int) / count_processes);
+
+    if(vector_size < 1) {
+        cout << "The amount of processes is higher than the amount of values. In this case not every process will become values." << endl;
+        return EXIT_FAILURE;
+    }
+    else if( (count_all_bytes / sizeof(int)) % count_processes != 0  ) {
+        cout << "The amount of values have to be even on every process. Otherwise the sorting would be incorrect." << endl;
+        return EXIT_FAILURE;
+    }
 
     // Allocate memory for vector.
-    vector->reserve((unsigned long) vector_size);
+    vector->reserve(vector_size);
 
     int actual_value;
     for(long int i = rank * sizeof(int);
         i < count_all_bytes;
-        i = i + sizeof(int) * count_nodes)
+        i = i + sizeof(int) * count_processes)
     {
         bin_file.seekg(i, ios::beg);
         bin_file.read(reinterpret_cast<char *>(&actual_value), sizeof(actual_value));
         vector->push_back(actual_value);
     }
+    return EXIT_SUCCESS;
 }
 
-/* print the data to the screen */
 void print(vector<int> *vector, int rank)
 {
     cout << "rank " << rank << " : ";
     for(unsigned long int i=0; i<vector->size(); i++)
-    {
         cout << vector->at(i) << " ";
-    }
     cout << endl;
 }
 
-/* do the parallel odd/even sort */
-void parallel_sort(vector<int> *data, int rank, int size)
-{
-    /* The data from our partner. */
-    vector<int> *other;
-    vector<int>::iterator dataIterator;
-    MPI_Status status;
-    int count_other;
+int findPartner(int phase, int rank) {
+    int partner;
 
-    /* we need to apply P phases where P is the number of processes */
-    for (int i=0; i<size; i++)
-    {
-        /* find our partner on this phase */
-        int partener;
-
-        /* if it's an even phase */
-        if (i % 2 == 0) {
-          /* if we are an even process */
-          if (rank % 2 == 0) {
-            partener = rank + 1;
-          } else {
-            partener = rank - 1;
-          }
+    /* if it's an even phase */
+    if (phase % 2 == 0) {
+        /* if we are an even process */
+        if (rank % 2 == 0) {
+            partner = rank + 1;
         } else {
-          /* it's an odd phase - do the opposite */
-          if (rank % 2 == 0) {
-            partener = rank - 1;
-          } else {
-            partener = rank + 1;
-          }
+            partner = rank - 1;
         }
+    } else {
+        /* it's an odd phase - do the opposite */
+        if (rank % 2 == 0) {
+            partner = rank - 1;
+        } else {
+            partner = rank + 1;
+        }
+    }
+    return partner;
+}
 
-        /* if the partener is invalid, we should simply move on to the next iteration */
-        if (partener < 0 || partener >= size) {
+void parallel_sort(vector<int> *data, int rank, int count_processes)
+{
+    const unsigned long vector_size = data->size();
+
+    vector<int> other(vector_size);
+    vector<int> concatVec(vector_size * 2);
+
+    for (int i=0; i<count_processes; i++)
+    {
+        int partner = findPartner(i, rank);
+        if (partner < 0 || partner >= count_processes)
           continue;
+
+        if (rank % 2 == 0) {
+            MPI_Send(&data->at(0), (int) vector_size, MPI_INT, partner, 0, MPI_COMM_WORLD);
+            MPI_Recv(&other.at(0), (int) vector_size, MPI_INT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        } else {
+            MPI_Recv(&other.at(0), (int) vector_size, MPI_INT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(&data->at(0), (int) vector_size, MPI_INT, partner, 0, MPI_COMM_WORLD);
         }
 
-        // get empty space for next round getting data from partner.
-        other = new vector<int>;
+        merge( data->begin(), data->end(), other.begin(), other.end(), concatVec.begin() );
+        sort(concatVec.begin(), concatVec.end());
 
-        /* do the exchange - even processes send first and odd processes receive first
-         * this avoids possible deadlock of two processes working together both sending */
-        if (rank % 2 == 0)
-        {
-            MPI_Send(&data->at(0), (int) data->size(), MPI_INT, partener, 0, MPI_COMM_WORLD);
-
-            // first get the count of elements from from partner, than alloc exat the needed space.
-            MPI_Probe(partener, 0, MPI_COMM_WORLD, &status);
-            MPI_Get_count(&status, MPI_INT, &count_other);
-            other->resize((unsigned long) count_other);
-
-            MPI_Recv( &other->at(0), count_other, MPI_INT, partener, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
+        auto posHalfVec = concatVec.begin() + vector_size;
+        if (rank < partner)
+            data->assign(concatVec.begin(), posHalfVec);
         else
-        {
-            // first get the count of elements from from partner, than alloc exat the needed space.
-            MPI_Probe(partener, 0, MPI_COMM_WORLD, &status);
-            MPI_Get_count(&status, MPI_INT, &count_other);
-            other->resize((unsigned long) count_other);
-
-            MPI_Recv(&other->at(0), count_other, MPI_INT, partener, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(&data->at(0), (int) data->size(), MPI_INT, partener, 0, MPI_COMM_WORLD);
-        }
-
-        /* now we need to merge data and other based on if we want smaller or larger ones */
-        if (rank < partener)
-        {
-            /* keep smaller keys */
-            for(long iOther = 0; iOther < other->size(); iOther++)
-            {
-                int actOtherValue = other->at((unsigned long) iOther);
-                if (actOtherValue < data->back())
-                {
-                    dataIterator = lower_bound(data->begin(), data->end(), actOtherValue);
-                    data->insert(dataIterator, actOtherValue);
-                    data->erase(data->end()-1);
-                }
-                else
-                    break;
-            }
-        }
-        else
-        {
-            /* keep larger keys */
-            for(long iOther = other->size()-1; iOther >= 0; iOther--)
-            {
-                int actOtherValue = other->at((unsigned long) iOther);
-                if (actOtherValue > data->front())
-                {
-                    dataIterator = upper_bound(data->begin(), data->end(), actOtherValue);
-                    data->insert(dataIterator, actOtherValue);
-                    data->erase(data->begin());
-                }
-                else
-                    break;
-            }
-        }
-        delete(other);
+            data->assign(posHalfVec, concatVec.end());
     }
 }
 
-
 /**
- * Compile: mpic++ OddEvenSort.cpp
+ * Compile: mpic++ OddEvenSort.cpp -o OddEvenSort -std=gnu++0x
  *
- * Example-Call: mpirun -n 4 ./a.out "<numbers_file.bin>" <y or nothing>
- * y == output on console
+ * Example-Call: mpirun -np 4 ./a.out "<numbers_file.bin>" <y>
+ * <y> output on console
+ * time mpirun -np 2 ./OddEvenSort "16 numbers - Sat Jan 20 14:02:43 2018.bin" y
  * */
 int main(int argCount, char** argValues)
 {
-    int rank, count_nodes;
+    int rank, count_processes;
 
     MPI_Init(&argCount, &argValues);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &count_nodes);
+    MPI_Comm_size(MPI_COMM_WORLD, &count_processes);
 
     vector<int> vec_data;
-    fill_vector_from_binary_file(&vec_data, argValues[1], rank, count_nodes);
+    int status = fill_vector_from_binary_file(&vec_data, argValues[1], rank, count_processes);
 
-    /* sort our local vector */
-    sort(vec_data.begin(), vec_data.end());
+    if (status == EXIT_FAILURE) {
+        MPI_Finalize();
+        return EXIT_FAILURE;
+    }
 
-    parallel_sort(&vec_data, rank, count_nodes);
+    parallel_sort(&vec_data, rank, count_processes);
 
-    if(argCount > 2 && strcmp(argValues[2], "y") == 0)
-    {
+    if(argCount > 2 && strcmp(argValues[2], "y") == 0) {
         print(&vec_data, rank);
     }
 
     MPI_Finalize();
-    return 0;
+    return EXIT_SUCCESS;
 }
