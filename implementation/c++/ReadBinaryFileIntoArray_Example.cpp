b2a19ac15f12cfa1edec1db4ffa390bcd8db28fc
eren <e.albayrak@it-frankfurt.com>
2018-01-22 20:59:08 +0100

    The whole file will read in on step now instead step by step. (huge performance boost!)

--- implementation/c++/ReadBinaryFileIntoArray_Example.cpp
+++ implementation/c++/ReadBinaryFileIntoArray_Example.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <cstring>
 
 using namespace std;
@@ -8,7 +7,8 @@ using namespace std;
 /**
  * This Example-Program read a binary file into a integer and print it out.
  * The content of the binary file should be also a integer array.
- * 
+ *
+ * Compile: g++ ReadBinaryFileIntoArray_Example.cpp -o ReadBinaryFileIntoArray_Example -std=gnu++0x
  * Example-Call with output: ./a.out "path/to/binayFile.bin" <y>
  */
 int main(int argCount, char *argValues[])
@@ -19,24 +19,16 @@ int main(int argCount, char *argValues[])
     myFile.seekg(0, ios::end);
 
     long int count_all_bytes = myFile.tellg();
-    int vec_size             = count_all_bytes / sizeof(int);
-    vector<int> vector(vec_size);
-    int a, iVec = 0;
+    int array_size           = count_all_bytes / sizeof(int);
+    int *array               = new int[array_size];
 
-    for(long int i=0; i<count_all_bytes; i = i + sizeof(int))
+    myFile.seekg(0, ios::beg);
+    myFile.read(reinterpret_cast<char *>(array), sizeof(int) * array_size);
+
+    if(argCount > 2 && strcmp(argValues[2], "y") == 0)
     {
-        myFile.seekg(i, ios::beg);
-        myFile.read(reinterpret_cast<char *>(&a), sizeof(a));
-        /*
-         * push_back do always a bad alloc exception if dealing with size > 1 GB
-         * vector.push_back(a);
-         * */
-        vector.at(iVec++) = a;
-    }
-
-    if(argCount > 2 && strcmp(argValues[2], "y") == 0) {
-        for (int j = 0; j < vector.size(); ++j) {
-            cout << vector.at(j) << " ";
+        for (int j = 0; j < array_size; ++j) {
+            cout << array[j] << " ";
         }
     }
     cout << endl;
